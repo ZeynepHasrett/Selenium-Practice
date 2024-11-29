9f8b43c52816d8b7213aebd2a9c4855b1c11aee1
ZeynepHasrett <158554030+ZeynepHasrett@users.noreply.github.com>
2024-11-29 10:48:55 +0300

    day01

--- src/tests/C01_IlkTest.java
+++ src/tests/C01_IlkTest.java
@@ -1,10 +1,19 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class C01_IlkTest {
 
     public static void main(String[] args) {
 
+        System.setProperty("Webdriver.chrome.driver", "kurulumDosyalari/chromedriver.exe");
 
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.babayigit.net");
+
+        driver.quit();
 
     }
 
